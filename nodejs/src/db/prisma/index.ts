3ffe4bcc42ee038ed70dfe1eb736d35d27d35eda
victor-lis <victorlisbronzo1@gmail.com>
2026-04-16 10:17:18 -0300

    fix(prisma): update prisma client initialization to use driver adapter (v7)

--- nodejs/src/db/prisma/index.ts
+++ nodejs/src/db/prisma/index.ts
@@ -1,3 +1,14 @@
+import "dotenv/config";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+const adapter = new PrismaMariaDb({
+  host: process.env.HOST || "127.0.0.1",
+  port: Number(process.env.PORT) || 3307,
+  user: process.env.USER || "root",
+  password: process.env.PASSWORD || "password",
+  database: process.env.DATABASE || "tasks_db",
+  connectionLimit: Number(process.env.CONNECTIONLIMIT) || 5,
+});
+
+export const prisma = new PrismaClient({ adapter });
